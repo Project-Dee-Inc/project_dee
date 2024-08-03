e3ae66d2218f390dd75aaf024c06845b587ac050
marBee42 <75686487+marBee42@users.noreply.github.com>
2024-08-03 21:07:48 +0800

    feat: Health Component

---
+++ Scripts/health_component.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+var max_health:int
+var current_health:int
+
+func _on_ready():
+	EventManager.add_listener(str(EventManager.EVENT_NAMES.ON_PLAYER_BASE_STATS_READY), self, "_get_health")
+
+# Damages!
+func _damage(value:int):
+	current_health = clamp(current_health - value, 0, max_health)
+
+# Heals!
+func _heal(value:int):
+	current_health = clamp(current_health + value, 0, max_health)
+
+func _get_health(value:Dictionary):
+	max_health = value[4]
+	
+
