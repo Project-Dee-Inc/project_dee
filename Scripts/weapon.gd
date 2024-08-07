789a4c9809770a53d673401cb707bd5f669e0169
TimHomes <eternalhaze01@gmail.com>
2024-08-07 22:26:21 +0800

    refactor: minor enum access changes

--- Scripts/weapon.gd
+++ Scripts/weapon.gd
@@ -6,7 +6,7 @@ var player_stat_dict: Dictionary = {}
 @onready var weapon_skill = $WeaponSkill
 	
 func _on_ready():
-	cooldown = weapon_skill.stat_dict[Constants.get_enum_value_by_name("CD")]
+	cooldown = weapon_skill.stat_dict[Constants.get_enum_name_by_value(Constants.STATS.CD)]
 	EventManager.add_listener(str(EventManager.EVENT_NAMES.ON_PLAYER_BASE_STATS_READY), self, "_get_player_stat") # Todo: Change this so it listens to "on weapon equipped" instead of "on player base stats ready"
 	
 func _get_player_stat(value: Dictionary):
