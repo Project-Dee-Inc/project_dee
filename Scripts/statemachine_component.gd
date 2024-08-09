563ab3974e93dbc03c0a638f93806d94cfc6c410
Jasmine G <75422215+janjas22@users.noreply.github.com>
2024-08-09 23:25:06 +0800

    feat: sample statemachine component with state scripts
    - Simple statemachine component
    - Has basic attack and follow state
    - Sample scripts just cycle from follow to basic attack every 0.5 seconds

--- Scripts/statemachine_component.gd
+++ Scripts/statemachine_component.gd
@@ -1,12 +1,36 @@
 extends Node
 class_name StateMachine
 
+@onready var movement_manager = $"../MovementComponent"
+@onready var skill_manager = $"../SkillManagerComponent"
 
-# Called when the node enters the scene tree for the first time.
+var current_state: Object
+
+var history = []
+var states = {}
+
 func _ready():
-	pass # Replace with function body.
+	for state in get_children():
+		state.fsm = self
+		state.movement_manager = movement_manager
+		state.skill_manager = skill_manager
+		states[state.name] = state
+		if current_state:
+			remove_child(state)
+		else:
+			current_state = state
+	current_state.enter()
 
+func change_to(state_name):
+	history.append(current_state.name)
+	set_state(state_name)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func back():
+	if history.size() > 0:
+		set_state(history.pop_back())
+
+func set_state(state_name):
+	remove_child(current_state)
+	current_state = states[state_name]
+	add_child(current_state)
+	current_state.enter()
