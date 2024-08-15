e1432902fc76ee95128674e7a951b68d76866cfb
Jasmine G <75422215+janjas22@users.noreply.github.com>
2024-08-15 23:25:46 +0800

    feat: updated fighter component
    - Added HP stat
    - New Surround state (just for testing)

--- Scripts/States/follow_state.gd
+++ Scripts/States/follow_state.gd
@@ -6,12 +6,12 @@ var skill_manager
 
 func enter():
 	print("In FOLLOWING state!")
+	movement_manager._set_surround(false)
 	movement_manager._state_moving(true)
 
-	# Exit 0.5 seconds later
-	await get_tree().create_timer(0.5).timeout
-	movement_manager._state_moving(false)
-	exit("BasicAttack")
+	# Exit 3 seconds later
+	await get_tree().create_timer(3).timeout
+	exit("Surround")
 
 func exit(next_state):
 	fsm.change_to(next_state)
