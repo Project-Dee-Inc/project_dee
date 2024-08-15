6f76b6af610304a8bf6188d65bd78edd56698665
Jasmine G <75422215+janjas22@users.noreply.github.com>
2024-08-15 23:27:24 +0800

    feat: basic game manager for event flow
    - Added basic autoloaded game manager
    - Starts with ON_START_GAME after 0.5 seconds
    - Receiving player then raises ON_PLAYER_INITIALIZED once done assigning basic values

---
+++ Scripts/game_manager.gd
@@ -0,0 +1,37 @@
+extends Node
+
+var player:Node3D
+
+func _ready():
+	on_subscribe_events()
+	on_game_start()
+
+# List events to listen for
+func on_subscribe_events():
+	EventManager.add_listener(str(EventManager.EVENT_NAMES.ON_PLAYER_INITIALIZED),self,"player_initialized")
+
+# Test run: start game after 0.5 seconds and raise ON_START_GAME event
+func on_game_start():
+	await get_tree().create_timer(0.5).timeout
+	print("STARTING GAME")
+	EventManager.raise_event(str(EventManager.EVENT_NAMES.ON_START_GAME), null)
+
+# Once player is initialized, find player node
+func player_initialized(params):
+	player = find_player_node()
+
+# Find player in all nested scenes to store global value
+func find_player_node() -> Node3D:
+	return find_node_recursive(get_tree().current_scene, "Player")
+
+func find_node_recursive(parent: Node, name: String) -> Node3D:
+	if parent.name == name and parent is Node3D:
+		return parent as Node3D
+
+	for child in parent.get_children():
+		if child is Node:
+			var result = find_node_recursive(child, name)
+			if result:
+				return result
+
+	return null
