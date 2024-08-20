a8ffcc89d46943aa3a10e49cf58e0407ab0ed53f
Jasmine G <75422215+janjas22@users.noreply.github.com>
2024-08-20 21:25:44 +0800

    refactor: cleaned script method names
    - Made script method uniform

--- Scripts/test_spawner.gd
+++ Scripts/test_spawner.gd
@@ -15,18 +15,18 @@ func _ready():
 func _start_spawning():
 	while true:
 		await get_tree().create_timer(spawn_delay).timeout
-		spawn_objects()
+		_spawn_objects()
 
-func spawn_objects():
+func _spawn_objects():
 	var target = GameManager.player.global_transform.origin
 	
 	# Attempt to spawn objects for each scene in the spawn table
 	for scene in spawn_table.keys():
 		if active_spawns[scene] < spawn_table[scene]:
-			var spawn_position = get_random_position_around_object(target, spawn_range)
-			spawn_object(scene, spawn_position)
+			var spawn_position = _get_random_position_around_object(target, spawn_range)
+			_spawn_object(scene, spawn_position)
 
-func spawn_object(scene: PackedScene, location: Vector3):
+func _spawn_object(scene: PackedScene, location: Vector3):
 	var instance = scene.instantiate()
 	instance.global_transform.origin = location
 	add_child(instance)
@@ -36,9 +36,9 @@ func spawn_object(scene: PackedScene, location: Vector3):
 
 func _on_object_destroyed(scene: PackedScene):
 	# When an object is destroyed, decrement the active count
-	active_spawns[scene] -- 1
+	active_spawns[scene] -= 1
 
-func get_random_position_around_object(object_position: Vector3, radius: float) -> Vector3:
+func _get_random_position_around_object(object_position: Vector3, radius: float) -> Vector3:
 	# Randomly pick a direction in 3D space (spherical coordinates)
 	var theta = randf_range(0.0, PI * 2)   # Random angle around Y-axis
 	var phi = randf_range(0.0, PI * 2)     # Random angle around XZ-plane
