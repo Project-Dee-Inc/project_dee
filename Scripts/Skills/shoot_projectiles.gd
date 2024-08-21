fc5e28138ac66e4c1999df96b547c4ff7d4a909a
Jasmine G <75422215+janjas22@users.noreply.github.com>
2024-08-22 01:29:41 +0800

    fix: instantiated colliders
    - Don't damage self
    - Also includes comments for some scripts
    - Instantiated collider layers are set to neutral so it's not detected as another enemy by others

--- Scripts/Skills/shoot_projectiles.gd
+++ Scripts/Skills/shoot_projectiles.gd
@@ -19,6 +19,7 @@ func _set_values():
 func _set_target(value:Node):
 	target = value
 
+# Set behavior for projectile, homing or normal
 func _set_homing(value:bool):
 	is_homing = value
 
@@ -28,11 +29,13 @@ func _activate_skill():
 func _deactivate_skill():
 	skill_is_active = false
 
+# If skill is active and within cd intervals, spawn projectile
 func _physics_process(_delta: float):
 	if (projectile_obj):
 		if(skill_is_active && can_activate):
 			_spawn_projectile()
 
+# Instantiate a copy of the base projectile scene
 func _spawn_projectile():
 	can_activate = false
 	var projectile = projectile_obj.instantiate() as Node3D
@@ -41,12 +44,17 @@ func _spawn_projectile():
 	projectile.global_transform.origin = base_node.global_transform.origin
 	get_parent().add_child(projectile)
 	projectile.scale = Vector3(0.5, 0.5, 0.5)
-	
+
+	# Set collision layer to 1, the same as environment
 	projectile._set_collision_layer(Constants.TARGETS.NEUTRAL)
+	# Set collision mask so that it only detects player
 	projectile._set_collision_masks(Constants.TARGETS.PLAYER)
 
+	# Set behavior if projectile is homing or not
 	projectile.is_homing = is_homing
+	# Start moving to target
 	projectile._shoot(projectile, target)
 
+	# Await for cd interval before looping and creating another instance
 	await get_tree().create_timer(cd).timeout
 	can_activate = true
--- Scripts/Skills/trapper_aoe_on_timeout.gd
+++ Scripts/Skills/trapper_aoe_on_timeout.gd
@@ -2,27 +2,50 @@ extends "res://Scripts/Skills/aoe_on_timeout.gd"
 class_name TrapperAoeOnTimeout
 
 @export var debuff_stat_component:Node
+@export var is_attack_or_debuff:bool
 var debuff_stat_dict:Dictionary = {}
 var debuff_value:float = 0
 var debuff_stat:Constants.STATS
 
+# Call base method and reference the debuff stat dictionary
+# If effect is for debuff, disable is_attack_or_debuff
 func _get_values():
 	super._get_values()
-	debuff_stat_dict = debuff_stat_component.stat_dict
+	if(!is_attack_or_debuff):
+		debuff_stat_dict = debuff_stat_component.stat_dict
 
+# Get values for which stat is debuffed and its value
 func _set_values():
 	super._set_values()
-	var key = debuff_stat_dict.keys()[1]
-	debuff_value = debuff_stat_dict[key]
-	debuff_stat = Constants.get_enum_value_by_name(key)
-	#print("KEY IS ", key)
-	#print("STAT IS ", debuff_stat)
+	if(!is_attack_or_debuff):
+		var key = debuff_stat_dict.keys()[1]
+		debuff_value = debuff_stat_dict[key]
+		debuff_stat = Constants.get_enum_value_by_name(key)
+		#print("KEY IS ", key)
+		#print("STAT IS ", debuff_stat)
 
 func _set_aoe_values(hit_collider:Area3D):
-	hit_collider._set_values(cd, debuff_value)
-	hit_collider._set_aoe_damage_type(false, debuff_stat)
+	# Get final value needed, if normal attack store damage, if debuff store debuff_value
+	var value 
+	if(is_attack_or_debuff):
+		value = damage
+		# Set to normal damage type
+		hit_collider._set_aoe_damage_type(true)
+	else:
+		value = debuff_value
+		# Set to debuff damage type and attach which stat it debuffs
+		hit_collider._set_aoe_damage_type(false, debuff_stat)
+
+	hit_collider._set_base_node(get_parent().parent_component)
+	# Set cd to despawn collider and the value it deals for damage or debuff
+	hit_collider._set_values(cd, value)
+	# Set sphere collider radius
 	hit_collider._set_collider_radius(radius)
-	hit_collider._set_collision_layer(Constants.TARGETS.ENEMY)
+	# Collision layer set to enemy
+	hit_collider._set_collision_layer(Constants.TARGETS.NEUTRAL)
+	# Should only scan player layer
 	hit_collider._set_collision_masks(Constants.TARGETS.PLAYER)
+	# Only enable character bodies since we're only dealing with the player
 	hit_collider._enable_character_bodies()
+	# Reenable collider and it despawns after set cd
 	hit_collider._enable_collider(true)
