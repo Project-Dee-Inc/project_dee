0be06eb56d0e18de13a5b03db13f80d087ba1d21
JunmaOgawa <junma_ogawa@dlsu.edu.ph>
2024-08-24 18:03:22 +0900

    Changing scenes for changing between levels
    can go from one scene to another
    [TODO] randomize levels, check current level, make sure that at level 5 it becomes a boss room.

--- Scripts/dungeon_teleporter.gd
+++ Scripts/dungeon_teleporter.gd
@@ -2,4 +2,9 @@ extends Area3D
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		print("collided with player")
+		var current_scene_file = get_tree().current_scene.scene_file_path
+		var next_level_number = current_scene_file.to_int() +1
+		
+		var next_level_path = "res://Scenes/Levels/level_test" + str(next_level_number) + ".tscn"
+		get_tree().change_scene_to_file(next_level_path)
+		print(next_level_path)
