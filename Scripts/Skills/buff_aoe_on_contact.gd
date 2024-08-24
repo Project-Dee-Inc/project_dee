bfaf4078fa29326046890462c5e66758e7ee2dea
Jasmine G <75422215+janjas22@users.noreply.github.com>
2024-08-25 00:42:30 +0800

    feat: new basic support enemy
    - Initial support enemy with basic state of following and finding the largest cluster of enemies
    - Haven't added the buffing skill yet

---
+++ Scripts/Skills/buff_aoe_on_contact.gd
@@ -0,0 +1,10 @@
+extends "res://Scripts/Skills/aoe_on_timeout.gd"
+class_name BuffAoe
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
