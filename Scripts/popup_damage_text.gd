73dd0ace5e9bf4fb527b97443011adbc586fa3e6
Jasmine G <75422215+janjas22@users.noreply.github.com>
2024-08-25 16:41:29 +0800

    feat: added popup damage text
    - Added pop up text when enemies are damaged
    - Added canvas to test scene

--- Scripts/popup_damage_text.gd
+++ Scripts/popup_damage_text.gd
@@ -1,29 +1,24 @@
 extends Node
 
 @export var damage_popup_scene:PackedScene
-@export var popup_duration:float = 0.5
+@export var popup_duration:float = 1
 var base_node:Node3D
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	#base_node = get_parent().get_parent()
-	#print("BASE NODE IS ", get_parent().get_parent().name)
-	#print("BASE NODE IS ", base_node.name)
-
-#func show_damage_popup(damage_amount:int):
-	## Instantiate the damage popup
-	#var popup_instance = damage_popup_scene.instantiate() as Label3D
-	#popup_instance.text = str(damage_amount)
-	#
-	## Add it to the scene tree
-	#get_tree().current_scene.add_child(popup_instance)
-#
-	## Position it slightly above the enemy
-	#popup_instance.global_transform.origin = base_node.global_transform.origin + Vector3(0, 2, 0)
-#
-	## Animate popup
-	#popup_instance.animate_popup(popup_instance)
+func _ready():
+	base_node = get_parent().get_parent()
 
 func _on_enemy_damaged(damage_value:int):
-	#show_damage_popup(damage_value)
-	print("DAMAGED FOR ", damage_value)
+	show_damage_popup(damage_value)
+
+func show_damage_popup(damage_amount:int):
+	# Instantiate the damage popup
+	var popup_instance = damage_popup_scene.instantiate() as Label3D
+	popup_instance.text = str(damage_amount)
+
+	# Position it slightly above the enemy
+	get_tree().current_scene.add_child(popup_instance)
+	popup_instance.global_transform.origin = base_node.global_transform.origin + Vector3(0, 1.5, 0)
+
+	# Animate
+	popup_instance._animate_popup_text(popup_instance, popup_duration)
