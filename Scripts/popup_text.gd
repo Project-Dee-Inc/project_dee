f5ed5f08bb1ff0c32e1a9301ce7380dd21c96c7e
Jasmine G <75422215+janjas22@users.noreply.github.com>
2024-08-25 16:49:02 +0800

    feat: edited enemy values

--- Scripts/popup_text.gd
+++ Scripts/popup_text.gd
@@ -6,6 +6,6 @@ func _ready():
 	tween = create_tween()
 
 func _animate_popup_text(popup_instance:Label3D, popup_duration:float):
-	tween.tween_property(popup_instance, "global_position:y", popup_instance.global_position.y + 2.0, popup_duration/2)
+	tween.tween_property(popup_instance, "global_position:y", popup_instance.global_position.y + 1.0, popup_duration/2)
 	tween.tween_property(popup_instance, "modulate:a", 0.0, popup_duration/2)
 	tween.tween_callback(Callable(popup_instance, "queue_free"))
