45ece65d2f3708744480407bc133d09f0b55ea4a
Ron-Eugine <82744948+Ron-Eugine@users.noreply.github.com>
2024-09-17 16:11:15 +0800

    refactor: reworked inventory system
    -The inventory now uses an item PackedScene instead of the weapon PackedScene, allowing the weapon and inventory display icons to be independent of each other.

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -4,9 +4,16 @@ const slot_class = preload("res://Scripts/slot.gd")
 @onready var grid_container: GridContainer = $GridContainer
 var selected_item = null
 
+var test_image = preload("res://Images/8.png")
+
 func _ready():
 	for inv_slot in grid_container.get_children():
 		inv_slot.connect("gui_input", Callable(self, "_slot_gui_input").bind(inv_slot))
+		
+	var item_instance = Constants.item_class.instantiate()
+	item_instance._display_item_icon("Test Item", test_image)
+	add_child(item_instance)
+	_store_item(item_instance)
 		
 func _slot_gui_input(event: InputEvent, slot: slot_class):
 	if event is InputEventMouseButton:
@@ -29,3 +36,10 @@ func _slot_gui_input(event: InputEvent, slot: slot_class):
 func _input(event):
 	if selected_item:
 		selected_item.global_position = get_global_mouse_position()
+		
+func _store_item(item):
+	for inv_slot in grid_container.get_children():
+		if !inv_slot.item:
+			inv_slot._drop_item(item)
+			break
+			
