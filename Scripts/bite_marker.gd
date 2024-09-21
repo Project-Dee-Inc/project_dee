d734e70a373bb1c5e2dcd332cb3a54879e584cc2
janjas22 <75422215+janjas22@users.noreply.github.com>
2024-09-22 05:39:17 +0800

    feat: adjusted values

--- Scripts/bite_marker.gd
+++ Scripts/bite_marker.gd
@@ -1,22 +1,28 @@
 extends StaticBody3D
 
 var moving:bool = false
+var speed:float = 10
 var target_pos:Vector3 = Vector3(0,0,0)
+
+func _set_speed(value:float):
+	speed = value
 
 func _set_target_position(position:Vector3):
 	target_pos = position
 	moving = true
 
 func _move_to_target_position(delta: float):
-	if(moving):
-		var direction = (target_pos - global_transform.origin).normalized()
-		global_transform.origin += global_transform.origin.lerp(direction * 5, 20 * delta)
+	if (moving):
+		var direction = (target_pos - self.global_transform.origin).normalized()
+		self.global_transform.origin += direction * speed * delta  # Adjust speed as needed
+
+		# Check if close enough to stop
+		if Constants.is_close_to_destination(self.global_transform.origin, target_pos, 0):
+			_stop_movement()
 
 func _stop_movement():
-	if(moving && Constants.is_close_to_destination(global_transform.origin, target_pos)):
-		moving = false
+	moving = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
 	_move_to_target_position(delta)
-	_stop_movement()
