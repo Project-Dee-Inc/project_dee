f5234242c3a243193bcac6250b20c0d535e72e49
janjas22 <75422215+janjas22@users.noreply.github.com>
2024-10-12 23:01:11 +0800

    feat: connected equipping and unequipping to active weapon manager
    - Still needs to be tested

--- Scripts/active_weapon_manager.gd
+++ Scripts/active_weapon_manager.gd
@@ -3,6 +3,7 @@ extends Node3D
 var active_weapons:Dictionary = {}
 var active_skills:Array[BaseSkill] = []
 var enable_active_skill:bool = true
+var max_size = 4
 
 func _ready():
 	_subscribe()
@@ -10,6 +11,8 @@ func _ready():
 
 func _subscribe():
 	EventManager.add_listener(str(EventManager.EVENT_NAMES.ON_ENABLE_SKILL_INPUT),self,"_enable_skill_input")
+	EventManager.add_listener(str(EventManager.EVENT_NAMES.ON_WEAPON_EQUIP), self, "equip_weapon")
+	EventManager.add_listener(str(EventManager.EVENT_NAMES.ON_WEAPON_UNEQUIP), self, "unequip_weapon")
 
 func _init_weapons():
 	active_weapons.clear()
@@ -21,6 +24,23 @@ func _init_weapons():
 
 func _enable_skill_input(param:Array):
 	enable_active_skill = param[0]
+
+func equip_weapon(_params):
+	# TO BE TESTED
+	if(active_weapons.size() < max_size):
+		var instance = _params[0]
+		add_child(instance)
+		instance.global_transform.origin = Vector3(0, 0, 0)
+		_init_weapons()
+
+func unequip_weapon(_params):
+	# TO BE TESTED
+	var instance = _params[0]
+	var active_instance = active_weapons[instance.name]
+	if(active_instance):
+		active_weapons.erase(active_instance.name)
+		active_instance.queue_free()
+		_init_weapons()
 
 func _process(delta):
 	if(enable_active_skill):
