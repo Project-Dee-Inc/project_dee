7829c94f659e7165505e9b11b3133edc0e25606b
janjas22 <75422215+janjas22@users.noreply.github.com>
2024-10-24 04:37:48 +0800

    feat: added changes for inventory and skill inputs

--- Scripts/skill_icon_manager.gd
+++ Scripts/skill_icon_manager.gd
@@ -14,34 +14,54 @@ extends Node
 
 # Variable to hold the current cooldown time in seconds; initialized to 0.0.
 var stat_cooldown:float = 0.0
-# Variable to hold the reference to the attached weapon; used for accessing weapon properties.
-var attached_weapon
+var is_enabled:bool = true
+var is_cd:bool = false
 
 func _ready():
-	# Call the _setup_timer function to initialize the timer and skill icon settings.
-	_setup_timer()
-	
+	_subscribe()
+	_set_values()
+
 	# Disable process.
 	set_process(false)
+
+func _subscribe():
+	EventManager.add_listener(str(EventManager.EVENT_NAMES.ON_ENABLE_SKILL_INPUT),self,"_enable_skill_input")
+
+func _set_values():
+	# Setup timer.
+	timer.wait_time = 5
+	skill_icon.max_value = timer.wait_time
 
 func _process(delta):
 	# Display cooldown timer.
 	cooldown.text = "%0.1f" % timer.time_left
 	skill_icon.value = timer.time_left
 
+func _enable_skill_input(param:Array):
+	is_enabled = param[0]
+	_set_full_visibility(is_enabled)
+
+func _set_full_visibility(value:bool):
+	if(value):
+		self.modulate.a = 1
+	else:
+		self.modulate.a = 0.5
+
 # Activate cooldown.
 func _activate():
+	is_cd = true
 	timer.start()
 	set_process(true)
 
 # Method for handling input events.
 func _input(event):
 	if event is InputEventKey:
-		if event.pressed and Input.is_action_pressed(skill_action):
+		if event.pressed && Input.is_action_pressed(skill_action) && is_enabled && !is_cd:
 			_activate()
 
 # Stop cooldown.
 func _on_timer_timeout():
+	is_cd = false
 	cooldown.text = ""
 	skill_icon.value = 0
 	set_process(false)
