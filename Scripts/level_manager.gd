02f48edd8b415043a5d919496152283d3c2efe18
janjas22 <75422215+janjas22@users.noreply.github.com>
2024-10-24 04:37:26 +0800

    feat: add in rooting of direction for player when held by mimic

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -1,5 +1,4 @@
 extends Node
 
 func _init():
-	EventManager.raise_event(str(EventManager.EVENT_NAMES.ON_ENABLE_SKILL_INPUT), [true])
 	EventManager.raise_event(str(EventManager.EVENT_NAMES.ON_PLAY), {})
