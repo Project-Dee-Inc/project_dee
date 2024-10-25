c0bf5881069dad9e5133623c5dda070895e505ce
janjas22 <75422215+janjas22@users.noreply.github.com>
2024-10-25 23:22:15 +0800

    feat: updated skill slots number

--- Scripts/skill_icon_manager.gd
+++ Scripts/skill_icon_manager.gd
@@ -6,14 +6,14 @@ extends Node
 @export var skill_action: String = "Skill1"
 # @export variable to define the name of the cooldown variable; can be set in the editor.
 @export var cooldown_variable_name: String = "base_cooldown"
+@export var skill_key: String = "1"
 
 @onready var skill_icon: TextureProgressBar = $Skill1/SkillIcon
 @onready var key: Label = $Skill1/Key
 @onready var cooldown: Label = $Skill1/Cooldown
 @onready var timer: Timer = $Skill1/Timer
 
-# Variable to hold the current cooldown time in seconds; initialized to 0.0.
-var stat_cooldown:float = 0.0
+var stat_cooldown:float = 5
 var is_enabled:bool = true
 var is_cd:bool = false
 
@@ -27,10 +27,14 @@ func _ready():
 func _subscribe():
 	EventManager.add_listener(str(EventManager.EVENT_NAMES.ON_ENABLE_SKILL_INPUT),self,"_enable_skill_input")
 
-func _set_values():
+func _set_cd(cd_value:float):
 	# Setup timer.
-	timer.wait_time = 5
+	timer.wait_time = cd_value
 	skill_icon.max_value = timer.wait_time
+
+func _set_values():
+	_set_cd(stat_cooldown)
+	key.text = skill_key
 
 func _process(_delta):
 	# Display cooldown timer.
